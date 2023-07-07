cc58eb0d3e85a5d166f6502182a9b5af4d20d004
sumin <ysm4260@gmail.com>
2023-07-07 17:41:34 +0900

    Feat : 게시물 리스트 보여주기, 게시물 상세페이지, 글 작성

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./common/Button";
+import Write from "../Main/Write";
+import shortid from "shortid";
+import Post from "../Main/Post";
 
 const Main = () => {
-  const divCard = { border: "1px solid black", padding: "10px", margin: "10px" };
+  const getToday = () => {
+    const today = new Date();
+    const year = ("0" + today.getFullYear()).slice(-2);
+    const month = ("0" + (today.getMonth() + 1)).slice(-2);
+    const date = ("0" + today.getDate()).slice(-2);
+    const hours = ("0" + today.getHours()).slice(-2);
+    const minutes = ("0" + today.getMinutes()).slice(-2);
+    return `${year}-${month}-${date} ${hours}:${minutes}`;
+  };
+
+  const [posts, setPosts] = useState([
+    {
+      postId: shortid.generate(),
+      title: "제목1",
+      body: "내용1",
+      userName: "몽식1",
+      date: getToday(),
+      kcal: "100",
+      exerciseHour: "30",
+      isDeleted: false,
+    },
+    {
+      postId: shortid.generate(),
+      title: "제목2",
+      body: "내용2",
+      userName: "몽식2",
+      date: getToday(),
+      kcal: "100",
+      exerciseHour: "30",
+      isDeleted: false,
+    },
+  ]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
   return (
     <>
       <div>
-        <button>글 작성하기</button>
+        <Button onClick={openModal}>글 작성하기</Button>
       </div>
-      <div>
-        <div style={divCard}>본문</div>
-        <div style={divCard}>본문</div>
-        <div style={divCard}>본문</div>
-      </div>
+      {isOpen && <Write setIsOpen={setIsOpen} setPosts={setPosts} getToday={getToday} posts={posts} />}
+      <Post posts={posts}></Post>
     </>
   );
 };
